94fe427f07567683016a5d26be790f0b7af1d31b
Ícaro Hoff <icarohoff@gmail.com>
2016-07-26 19:29:44 -0300

    g2-common: adjust to new vendor path [2/2]
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- common.mk
+++ common.mk
@@ -15,7 +15,7 @@
 #
 
 # Call common vendor
-$(call inherit-product, vendor/galbi/g2-common/g2-common-vendor.mk)
+$(call inherit-product, vendor/lge/g2-common/g2-common-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
