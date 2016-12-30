c982ad335c1c22f4d9cd2af3826b555cc3515953
Ícaro Hoff <icarohoff@gmail.com>
2016-12-30 10:01:45 -0200

    g2-common: performance and SELinux fixes
    A general bring up for SELinux on enforcing mode. Also, disabled custom
    hotplug to allow mpdecision to control the plugging since MSM limiter
    supports it, it won't control frequencies...
    
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- rootdir/etc/lambda-post_boot.sh
+++ rootdir/etc/lambda-post_boot.sh
@@ -11,12 +11,14 @@
 ############################
 # Kick-off
 #
+sleep 1
 stop mpdecision # To avoid troubles...
-echo "[Lambda] Kicking-off post-boot script" | tee /dev/kmsg
+echo "[λ] Kicking-off post-boot script" | tee /dev/kmsg
 
 ############################
 # Simple Thermal
 #
+sleep 1
 echo "0" > /sys/kernel/msm_thermal/enabled
 echo "1728000 39 38" > /sys/kernel/msm_thermal/zone0
 echo "1574400 42 40" > /sys/kernel/msm_thermal/zone1
@@ -30,27 +32,19 @@ echo "8000" > /sys/kernel/msm_thermal/sampling_ms
 echo "1" > /sys/kernel/msm_thermal/enabled
 
 ############################
-# RQ Stats
-#
-echo "0" > /sys/devices/system/cpu/cpu0/rq-stats/hotplug_enable
-
-############################
-# MSM Hotplug
-#
-echo "1" > /sys/module/msm_hotplug/msm_enabled
-
-############################
 # MSM Limiter
 #
-echo "2265600" > /sys/kernel/msm_limiter/resume_max_freq
-echo "0" > /sys/kernel/msm_limiter/mpd_enabled
+sleep 1
+echo "1" > /sys/kernel/msm_limiter/mpd_enabled
 echo "1" > /sys/kernel/msm_limiter/debug_mask
 echo "1" > /sys/kernel/msm_limiter/freq_control
-echo "ondemand" > /sys/kernel/msm_limiter/scaling_governor
+echo "2265600" > /sys/kernel/msm_limiter/resume_max_freq
+echo "interactive" > /sys/kernel/msm_limiter/scaling_governor
 
 ############################
 # CPU Input Boost
 #
+sleep 0.5
 echo "1190400 1497600" > /sys/kernel/cpu_input_boost/ib_freqs
 echo "1400" > /sys/kernel/cpu_input_boost/ib_duration_ms
 echo "1" > /sys/kernel/cpu_input_boost/enabled
@@ -58,16 +52,19 @@ echo "1" > /sys/kernel/cpu_input_boost/enabled
 ############################
 # I/O Scheduler
 #
+sleep 0.5
 echo "fiops" > /sys/block/mmcblk0/queue/scheduler
 
 ############################
 # Swappiness
 #
+sleep 1
 echo "50" > /proc/sys/vm/swappiness
 
 ############################
 # LMK
 #
+sleep 1
 echo "18432,23040,24576,28672,31744,34816" > /sys/module/lowmemorykiller/parameters/minfree
 echo "48" > /sys/module/lowmemorykiller/parameters/cost
 echo "73728" > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
@@ -79,6 +76,7 @@ echo "0" > /sys/module/lowmemorykiller/parameters/debug_level
 ############################
 # Debugging
 #
+sleep 1
 echo "0" > /sys/module/alarm_dev/parameters/debug_mask
 echo "0" > /sys/module/lge_touch_core/parameters/debug_mask
 echo "0" > /sys/devices/fe12f000.slim/debug_mask
@@ -93,4 +91,6 @@ echo "0" > /sys/module/msm_pm/parameters/debug_mask
 ############################
 # Finish
 #
-echo "[Lambda] Exiting post-boot script" | tee /dev/kmsg
+sleep 3
+echo "[λ] Exiting post-boot script" | tee /dev/kmsg
+start mpdecision
